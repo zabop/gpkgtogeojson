1d69697bb4bc3c813470c32f7bb4c2be775a1d45
zabop <szabopal96@gmail.com>
2024-04-09 23:02:11 +0200

    complete minimal attempt

---
+++ frontend/src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useState } from 'react';
+import './App.css'; // Import the CSS file
+import * as FileSaver from 'file-saver'; // Import FileSaver.js
+
+const App = () => {
+  const hiddenFileInput = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [buttonText, setButtonText] = useState('Select GPKG file'); // Added state for button text
+
+  const handleFileChange = (event) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setSelectedFile(file);
+      setButtonText(`Transforming: ${file.name}`); // Update button text
+      readFileContent(file); // Read the file content and send to server
+    }
+  };
+
+  const handleClick = () => {
+    hiddenFileInput.current?.click();
+  };
+
+  const readFileContent = async (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const base64Content = reader.result.split(',')[1]; // Extract base64 data
+      const dataToSend = {
+        name: file.name,
+        base64src: base64Content,
+      };
+      sendFileToServer(dataToSend, () => setButtonText('Convert another GPKG')); // Send the constructed JSON object
+    };
+  };
+
+  const sendFileToServer = async (data,_callback) => {
+    try {
+      const response = await fetch('https://gpkgtogeojson-backend.fly.dev/gpkg', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('File data sent successfully!');
+
+        // Use FileSaver.js to download the response
+        const blob = await response.blob();
+        FileSaver.saveAs(blob, 'dst.geojson')
+
+        setButtonText('Downloading GeoJSON'); // Update button text for download
+      } else {
+        console.error('Error sending file data:', response.statusText);
+        // Handle any errors that occur during the request
+      }
+    } catch (error) {
+      console.error('Error sending file data:', error);
+      // Handle any errors during the fetch operation
+    }
+    _callback();
+  };
+
+  return (
+    <div className="App">
+      <button
+        id="gpkg-file-button"
+        onClick={handleClick}
+        className="colored-button"
+      >
+        {buttonText}
+      </button>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+};
+
+export default App;
